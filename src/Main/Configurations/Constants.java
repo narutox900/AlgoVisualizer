015b48101a923ce356a031381118bc8b97f759f6
Anh <anhduy0911@gmail.com>
2020-05-30 10:19:12 +0700

    Controller modified, map created

---
+++ src/Main/Configurations/Constants.java
@@ -0,0 +1,15 @@
+package Main.Configurations;
+
+// Class contains all the constant used through out the project
+public class Constants
+{
+    public static final int ROW = 25, COL = 40;
+
+    //Colors
+    public static final String BORDER = "black";
+    public static final String WALL = "#202040";
+    public static final String UNVISITED = "#CFF1EF";
+    public static final String SOURCE = "#F9D89C";
+    public static final String TARGET = "#BC658D";
+
+}
